c26bbc64eb2d50da0658f9a34576c0316ac04bbe
justenoneal <justenoneal@gmail.com>
2023-02-27 15:48:21 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+def hello():
+    print("Hello")
+
+def pack(a, b, c):
+    newList = [a, b, c]
+    print(newList)
+
+def eat_lunch(lunchList):
+    print("First I eat", lunchList[0])
+    if len(lunchList) > 1:
+        i = 1
+        while i < len(lunchList):
+            print("Next I eat", lunchList[i])
+            i += 1
+    return None
+
+hello()
+pack(1, 2, 3)
+eat_lunch(["a hamburger", "fries", "cheese cake"])
